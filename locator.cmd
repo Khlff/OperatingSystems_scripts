c53ca6b6a512a9700dc34199e7fb476fedc25b3b
Khlff <75330347+Khlff@users.noreply.github.com>
2021-12-30 23:25:48 +0500

    Update locator.cmd

--- locator.cmd
+++ locator.cmd
@@ -1,5 +1,5 @@
 @echo off
-setLocal EnableDelayedExpansion
+
 if "%~1"=="" (
     echo To output the help, pass the argument "/?".
     goto exit
@@ -10,43 +10,50 @@ if "%~1"=="/?" (
 	goto exit
 )
 
-help | findstr /B /I "%~1\>" 
-if %ERRORLEVEL% == 0 (
-    if not exist C:\Windows\System32\%~1 (
-		echo Internal command
-        goto exit
-	)
-) else (
-	if exist %CD%\%~1 (
-		echo %CD%\%~1
-		echo It`s external command
-		goto exit
-	) else (
-		for %%K in ("%PATHEXT:;=" "%") do (
-			if exist "%CD%\%~1%%~K" (
-				echo %CD%\%~1%%~K
-				echo It`s external command
-				goto exit
-				)
-			)
-		)
-	
-	for /D %%B in ("%PATH:;=" "%") do (
-		if exist %%~B\%~1 (
-			echo %%~B\%~1
-			echo It`s external command
-			goto exit
-		) else (
-			for %%K in ("%PATHEXT:;=" "%") do (
-				if exist "%%~B\%~1%%~K" (
-					echo %%~B\%~1%%~K
-					echo It`s external command
-					goto exit
-				)
-			)
-		)
-	)
-	echo Error: not found 	
+help | findstr /B /I "%~1" > nul
+
+if exist "%CD%\%~1\*" (
+	echo It`s a folder!
+	goto exit
 )
 
+if %ERRORLEVEL% == 0 (
+	if exist C:\Windows\System32\%~1.* (
+		echo External command
+		goto exit
+	) else (
+		echo Internal command
+		goto exit
+    )
+)
+
+
+if exist %CD%\%~1 (
+	echo In this directory
+	echo %CD%\%~1
+	goto exit
+)
+
+for %%K in ("%PATHEXT:;=" "%") do (
+	if exist "%CD%\%~1%%~K" (
+		echo %CD%\%~1%%~K
+		echo In this directory
+		goto exit
+	)
+)
+
+for /D %%B in ("%PATH:;=" "%") do (
+	for %%K in ("%PATHEXT:;=" "%") do (
+		if exist "%%~B\%~1%%~K" (
+			echo %%~B\%~1%%~K
+			goto exit
+		)
+	)
+	if exist %%~B\%~1 (
+		echo %%~B\%~1
+		echo It`s external command
+		goto exit
+	)
+)
+echo Error: not found 	
 :exit
